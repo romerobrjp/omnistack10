ab0daba936e6a0b2e92259eea8b108a79e160adc
Romero Medeiros <romeromedeiros5@gmail.com>
2020-01-18 12:57:48 -0300

    Adds endpoint to retrieve github user info

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
+const axios = require('axios');
 
 const routes = Router();
 
 routes.get('/', (req, res) => {
   console.log(req.body);
   return res.json({message: 'Hello Omnistack 10!'});
+});
+
+routes.get('/devs/:username', async (req, res) => {
+  const githubUsername = req.params.username;
+  const githubResponse = await axios.get(`https://api.github.com/users/${githubUsername}`);
+  return res.json(githubResponse.data);
 });
 
 module.exports = routes;
