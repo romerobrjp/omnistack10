7de02aec592e1828388b47b3b2556664b5a53bbf
Romero Medeiros <romeromedeiros5@gmail.com>
2020-01-18 17:17:33 -0300

    Connects to mongo and tell express to use json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const routes = require('./routes');
 
 const app = express();
 
-app.use(express.json())
+mongoose.connect('mongodb+srv://<login>:<password>@cluster0-fhv5g.mongodb.net/omnistack10?retryWrites=true&w=majority', {
+  useNewUrlParser: true, // current URL string parser is deprecated, so use the new one
+  useUnifiedTopology: true // current Server Discovery and Monitoring engine is deprecated, so use new one
+});
+
+app.use(express.json()) // Tell express how to handle json data
 app.use(routes);
 
 app.listen(3333);
