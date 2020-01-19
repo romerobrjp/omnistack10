c1dec4a6af5e8606bf272c65446a4e7dd0f882f2
Romero Medeiros <romeromedeiros5@gmail.com>
2020-01-18 23:29:58 -0300

    Adds some routes and reorganizes code

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 const { Router } = require('express');
 const axios = require('axios');
-const DevController = require('./controllers/DevController')
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
+// Dev
 routes.get('/', DevController.hello);
-
 routes.get('/devs/:username', DevController.getOne);
-
+routes.get('/devs', DevController.getAll);
 routes.post('/devs', DevController.create);
+
+// Search
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
