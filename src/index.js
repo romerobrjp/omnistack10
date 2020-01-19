d52da1d16e7a4883cb68a7aea793b9ea6881d63e
Romero Medeiros <romeromedeiros5@gmail.com>
2020-01-18 22:31:37 -0300

    Uses the credentials file to fill the connection string

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const routes = require('./routes');
+const creds = require('./creds')
 
 const app = express();
 
-mongoose.connect('mongodb+srv://<login>:<password>@cluster0-fhv5g.mongodb.net/omnistack10?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${creds.mongodb.username}:${creds.mongodb.password}@cluster0-fhv5g.mongodb.net/omnistack10?retryWrites=true&w=majority`, {
   useNewUrlParser: true, // current URL string parser is deprecated, so use the new one
   useUnifiedTopology: true // current Server Discovery and Monitoring engine is deprecated, so use new one
 });
